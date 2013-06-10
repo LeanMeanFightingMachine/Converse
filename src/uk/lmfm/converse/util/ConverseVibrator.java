77fcb4589cb319ba907f448306fc78c69fe41985
Niall Gunter <namgdev@gmail.com>
2013-06-10 10:17:41 +0100

    Configuration changes.

--- src/uk/lmfm/converse/util/ConverseVibrator.java
+++ src/uk/lmfm/converse/util/ConverseVibrator.java
@@ -19,21 +19,21 @@ public class ConverseVibrator {
 		
 		String lower = s.toLowerCase(Locale.getDefault());
 		
-		if (lower.contains("turn") && lower.contains("right")) {
+		if (lower.contains("right")) {
 			VIBRATOR.vibrate(RIGHT, -1);
 		}
-		if (lower.contains("turn") && lower.contains("left")) {
+		if (lower.contains("left")) {
 			VIBRATOR.vibrate(LEFT, -1);
 		}
 		if (lower.contains("continue")) {
 			VIBRATOR.vibrate(CONT, -1);
 		}
 
-		if (lower.contains("head") && lower.contains("north")) {
+		if (lower.contains("north")) {
 			VIBRATOR.vibrate(NORTH, -1);
 		}
 
-		if (lower.contains("head") && lower.contains("south")) {
+		if (lower.contains("south")) {
 			VIBRATOR.vibrate(SOUTH, -1);
 		}
 
